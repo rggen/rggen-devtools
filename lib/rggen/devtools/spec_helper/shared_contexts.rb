ed17db47125cb0d28fb431e41123f61e66cc7747
taichi <taichi730@gmail.com>
2019-07-18 16:33:04 +0900

    added 'sv ral common' shared context

--- lib/rggen/devtools/spec_helper/shared_contexts.rb
+++ lib/rggen/devtools/spec_helper/shared_contexts.rb
@@ -168,3 +168,38 @@ RSpec.shared_context 'sv rtl common' do
     @sv_rtl_facotry ||= []
   end
 end
+
+RSpec.shared_context 'sv ral common' do
+  include_context 'configuration common'
+  include_context 'register map common'
+
+  def build_sv_ral_factory(builder)
+    builder.build_factory(:output, :sv_ral)
+  end
+
+  def create_sv_ral(configuration = nil, &data_block)
+    register_map = create_register_map(configuration) do
+      register_block(&data_block)
+    end
+    @sv_ral_factory[0] ||= build_sv_ral_factory(RgGen.builder)
+    @sv_ral_factory[0].create(configuration || default_configuration, register_map)
+  end
+
+  def delete_sv_ral_factory
+    @sv_ral_factory.clear
+  end
+
+  def have_variable(domain, handler, **attributes, &body)
+    variable = RgGen::SystemVerilog::Utility::DataObject.new(:variable, **attributes, &body)
+    have_declaration(domain, :variable, variable.declaration).and have_identifier(handler, variable.identifier)
+  end
+
+  def have_parameter(domain, handler, **attributes, &body)
+    parameter = RgGen::SystemVerilog::Utility::DataObject.new(:parameter, **attributes, &body)
+    have_declaration(domain, :parameter, parameter.declaration).and have_identifier(handler, parameter.identifier)
+  end
+
+  before(:all) do
+    @sv_ral_factory ||= []
+  end
+end
