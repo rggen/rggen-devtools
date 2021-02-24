82580e19455f375d4a77e168b9bfceab51a74059
Taichi Ishitani <taichi730@gmail.com>
2021-02-24 23:20:48 +0900

    add script to run all spec

---
+++ bin/run_all_spec.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+
+require 'fileutils'
+require 'optparse'
+require File.expand_path('../lib/rggen/devtools/checkout_list_utils', __dir__)
+extend RgGen::Devtools::CheckoutListUtils
+
+def join_path(*item)
+  File.join(*item.compact)
+end
+
+options = ARGV.getopts('', 'root:')
+root = options['root']
+
+list = join_path(root, 'rggen-checkout', 'all.yml')
+read_checkout_list(list).each do |repository, _|
+  FileUtils.cd(join_path(root, File.basename(repository))) do
+    if File.exist?('Rakefile')
+      command = 'rake spec'
+      puts command
+      system(command) || abort
+    end
+  end
+end
