76738d54b40c4ea7e98a8b0df87212a36b00cdb4
Taichi Ishitani <taichi730@gmail.com>
2021-02-24 23:29:02 +0900

    checkout repository only when it does not exist

--- bin/checkout.rb
+++ bin/checkout.rb
@@ -8,7 +8,11 @@ extend RgGen::Devtools::CheckoutListUtils
 options = ARGV.getopts('', 'list:', 'dir:')
 
 read_checkout_list(options['list'])&.each do |repository, branch|
-  command = "git clone --branch=#{branch} https://github.com/#{repository}.git"
-  puts command
-  FileUtils.cd(options['dir'] || rggen_root) { system(command) || abort }
+  FileUtils.cd(options['dir'] || rggen_root) do
+    unless Dir.exist?(File.basename(repository))
+      command = "git clone --branch=#{branch} https://github.com/#{repository}.git"
+      puts command
+      system(command) || abort
+    end
+  end
 end
