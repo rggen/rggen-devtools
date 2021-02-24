60a1a2444906d1282a82bc89298b522450e3e6e4
Taichi Ishitani <taichi730@gmail.com>
2021-02-24 22:23:04 +0900

    change current directory to rggen_root before cloning rggen libraries

--- bin/checkout.rb
+++ bin/checkout.rb
@@ -1,10 +1,11 @@
 #! /usr/bin/env ruby
 
+require 'fileutils'
 require File.expand_path('../lib/rggen/devtools/checkout_list_utils', __dir__)
 extend RgGen::Devtools::CheckoutListUtils
 
 read_checkout_list&.each do |repository, branch|
   command = "git clone --branch=#{branch} https://github.com/#{repository}.git"
   puts command
-  system(command) || abort
+  FileUtils.cd(rggen_root) { system(command) || abort }
 end
