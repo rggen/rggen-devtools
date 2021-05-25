5ce54c4c9ff87e3eb1e4497046345caee0dd9ba1
Taichi Ishitani <taichi730@gmail.com>
2021-05-25 09:20:20 +0900

    add '--ssh' option

--- bin/checkout.rb
+++ bin/checkout.rb
@@ -5,12 +5,18 @@ require 'optparse'
 require File.expand_path('../lib/rggen/devtools/checkout_list_utils', __dir__)
 extend RgGen::Devtools::CheckoutListUtils
 
-options = ARGV.getopts('', 'list:', 'dir:')
+options = ARGV.getopts('', 'list:', 'dir:', 'ssh')
 
 read_checkout_list(options['list'])&.each do |repository, branch|
   FileUtils.cd(options['dir'] || rggen_root) do
     unless Dir.exist?(File.basename(repository))
-      command = "git clone --branch=#{branch} https://github.com/#{repository}.git"
+      url =
+        if options['ssh']
+          "git@github.com:#{repository}.git"
+        else
+          "https://github.com/#{repository}.git"
+        end
+      command = "git clone --branch=#{branch} #{url}"
       puts command
       system(command) || abort
     end
