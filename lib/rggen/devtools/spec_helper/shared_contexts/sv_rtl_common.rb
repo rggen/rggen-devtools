f9911ac228705e8af9a9dc9ca0a69c42a61ec9ed
Taichi Ishitani <taichi730@gmail.com>
2025-12-21 19:58:11 +0900

    add `not_have_parameter` matcher

--- lib/rggen/devtools/spec_helper/shared_contexts/sv_rtl_common.rb
+++ lib/rggen/devtools/spec_helper/shared_contexts/sv_rtl_common.rb
@@ -111,6 +111,19 @@ RSpec.shared_context 'sv rtl common' do
     have_declaration(layer, :parameter, parameter.declaration).and have_identifier(handler, parameter.identifier)
   end
 
+  def not_have_parameter(*args, &body)
+    layer, handler, attributes =
+      if args.size == 3
+        args[0..2]
+      elsif args.size == 2
+        [nil, *args[0..1]]
+      else
+        [nil, args[0], {}]
+      end
+    parameter = RgGen::SystemVerilog::Common::Utility::DataObject.new(:parameter, **attributes, &body)
+    not_have_declaration(layer, :parameter, parameter.declaration).and not_have_identifier(handler, parameter.identifier)
+  end
+
   before(:all) do
     @sv_rtl_factory ||= []
   end
